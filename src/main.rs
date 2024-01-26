305349c612ebe1c293e5cf3335fb113276450b38
ale <malejlopez@hotmail.com>
2024-01-26 02:21:22 -0400

    Goto handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use std::fs::File;
+use std::{env, io::Error};
+use thiserror::Error;
+
 
 fn main() -> Result<(), std::io::Error> {
-    let f = File::open("bar.txt")?;
+    let args: Vec<String> = env::args().collect();
 
-    Ok(())
+    match args.len() {
+        1 => {println!("run File")Ok(())},
+        0 => {println!("REPL");Ok(())},
+        _=>Err(anyhow!("asd"))
+    }
+
 }
